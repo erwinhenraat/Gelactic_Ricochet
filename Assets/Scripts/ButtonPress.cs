a446daad62afca6efb76dc64a6570cf09461a46b
ruiter <38767@ma-web.nl>
2026-02-18 10:18:32 +0100

    Button that changes the scene

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour
+{
+    public void SceneChanger()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
